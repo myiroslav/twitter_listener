39585c44754f779a3723a55b41ef20d457efd242
Pierre Sibut-Bourde <pierre.sibut.bourde@gmail.com>
2022-08-03 19:45:46 -0400

    added simple function

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
-# twitter_listener
+# Twitter Listener
+
 A twitter bot to listen to certain users
 The objective is to follow some bots on twitter that share their calls, and to replicate them on a trading exchange. 
+
+## Setting up the .env file
+You can set up the .env file with the following syntax:
+```
+TWITTER_CONSUMER_KEY=<your_consumer_key>
+TWITTER_CONSUMER_SECRET=<your_consumer_secret>
+TWITTER_ACCESS_TOKEN=<your_access_token>
+TWITTER_ACCESS_TOKEN_SECRET=<your_access_token_secret>
+BEARER_TOKEN=<your_bearer_token>
+```
+It will be used to authenticate on Twitter, using load_dotenv(). Of course, this file is git ignored for security reasons.
--- main.py
+++ main.py
@@ -1,9 +1,42 @@
 from dotenv import load_dotenv
+from typing import Optional
 import os
-import tweepy
+import requests
 
 load_dotenv()
-BEARER = os.getenv("BEARER")
+BEARER = os.getenv("BEARER_TOKEN")
+
+def get_data(screen_name: str, count: int) -> Optional[dict]:
+    """
+    Get data from Twitter API
+    
+    Parameters
+    ----------
+    screen_name : str
+        Twitter screen name
+    count : int
+        Number of tweets to get
+    
+    Returns
+    -------
+    dict : Optional[dict] (Union[None, dict])
+        Dictionary containing the data from the API
+    """
+    url = f"https://api.twitter.com/1.1/statuses/user_timeline.json"
+    params = {"screen_name": screen_name, "count": count}
+    headers = {"Authorization": f"Bearer {BEARER}"}
+    response = requests.get(url, params=params, headers=headers)
+    return response.json()
+
+def main() -> None:
+    """
+    Main function
+    """
+    screen_name = "LogarithmeNeper"
+    count = 10
+    data = get_data(screen_name, count)
+    if data is not None:
+        print(data)
 
 if __name__ == "__main__":
-    print(BEARER)
+    main()
